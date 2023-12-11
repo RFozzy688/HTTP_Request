fb30de569d6a41aced716503e76400e8131308b0
RFozzy688 <pokerfozzy@gmail.com>
2023-12-11 23:11:57 +0200

    погода sinoptik.ua

--- HTTP_Request/MainWindow.xaml.cs
+++ HTTP_Request/MainWindow.xaml.cs
@@ -23,11 +23,70 @@ namespace HTTP_Request
         string _strSearch;
         string _strUri;
         string _searchSystem;
+        List<string> _towns;
+        string _selectedObl;
+        string _selectedTown;
         public MainWindow()
         {
             InitializeComponent();
 
             _searchSystem = "Google";
+
+            _towns = new List<string>();
+            _towns.Add("Сімферополь");
+            _towns.Add("Вінниця");
+            _towns.Add("Луцьк");
+            _towns.Add("Дніпро");
+            _towns.Add("Донецьк");
+            _towns.Add("Житомир");
+            _towns.Add("Ужгород");
+            _towns.Add("Запоріжжя");
+            _towns.Add("Івано-Франківськ");
+            _towns.Add("Київ");
+            _towns.Add("Кропивницький");
+            _towns.Add("Луганськ");
+            _towns.Add("Львів");
+            _towns.Add("Миколаїв");
+            _towns.Add("Одеса");
+            _towns.Add("Полтава");
+            _towns.Add("Рівне");
+            _towns.Add("Суми");
+            _towns.Add("Тернопіль");
+            _towns.Add("Харків");
+            _towns.Add("Херсон");
+            _towns.Add("Хмельницький");
+            _towns.Add("Черкаси");
+            _towns.Add("Чернівці");
+            _towns.Add("Чернігів");
+
+            NameObl.Items.Add("АР Крим");
+            NameObl.Items.Add("Вінницька область");
+            NameObl.Items.Add("Волинська область");
+            NameObl.Items.Add("Дніпропетровська область");
+            NameObl.Items.Add("Донецька область");
+            NameObl.Items.Add("Житомирська область");
+            NameObl.Items.Add("Закарпатська область");
+            NameObl.Items.Add("Запорізька область");
+            NameObl.Items.Add("Івано - Франківська область");
+            NameObl.Items.Add("Київська область");
+            NameObl.Items.Add("Кіровоградська область");
+            NameObl.Items.Add("Луганська область");
+            NameObl.Items.Add("Львівська область");
+            NameObl.Items.Add("Миколаївська область");
+            NameObl.Items.Add("Одеська область");
+            NameObl.Items.Add("Полтавська область");
+            NameObl.Items.Add("Рівненська область");
+            NameObl.Items.Add("Сумська область");
+            NameObl.Items.Add("Тернопільська область");
+            NameObl.Items.Add("Харківська область");
+            NameObl.Items.Add("Херсонська область");
+            NameObl.Items.Add("Хмельницька область");
+            NameObl.Items.Add("Черкаська область");
+            NameObl.Items.Add("Чернівецька область");
+            NameObl.Items.Add("Чернігівська область");
+
+            _selectedObl = string.Empty;
+            _selectedTown = string.Empty;
         }
 
         private void Search_Click(object sender, RoutedEventArgs e)
@@ -85,6 +144,51 @@ namespace HTTP_Request
         private void Bing_Click(object sender, RoutedEventArgs e)
         {
             _searchSystem = "Bing";
+        }
+        private void NameObl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (SevenDays.IsChecked == true)
+            {
+                SevenDays_Click(null, null);
+            }
+            else if (TenDays.IsChecked == true)
+            {
+                TenDays_Click(null, null);
+            }
+        }
+        private void MyTabControl_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            NameObl.SelectedIndex = 14;
+            SevenDays.IsChecked = true;
+
+            if (MyTabControl.SelectedIndex == 1)
+            {
+                Browser.Navigate(new Uri($"https://ua.sinoptik.ua/погода-{_towns[NameObl.SelectedIndex]}"));
+            }
+            else
+            {
+                Browser.Navigate((Uri)null);
+            }
+        }
+        private void WeatherOnMap_Click(object sender, RoutedEventArgs e)
+        {
+            if (NameObl.SelectedIndex != -1)
+            {
+                _selectedObl = NameObl.Items[NameObl.SelectedIndex].ToString().Replace(' ', '-').ToLower();
+                Browser.Navigate(new Uri($"https://ua.sinoptik.ua/україна/{_selectedObl}"));
+            }
+            else
+            {
+                Browser.Navigate(new Uri($"https://ua.sinoptik.ua/україна"));
+            }
+        }
+        private void SevenDays_Click(object sender, RoutedEventArgs e)
+        {
+            Browser.Navigate(new Uri($"https://ua.sinoptik.ua/погода-{_towns[NameObl.SelectedIndex]}"));
+        }
+        private void TenDays_Click(object sender, RoutedEventArgs e)
+        {
+            Browser.Navigate(new Uri($"https://ua.sinoptik.ua/погода-{_towns[NameObl.SelectedIndex]}/10-днів"));
         }
     }
 }
