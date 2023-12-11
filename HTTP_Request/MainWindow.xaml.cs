a8ea4ad52a4d46a845041cc7f940e08a6611b7af
RFozzy688 <pokerfozzy@gmail.com>
2023-12-11 18:52:22 +0200

    поиск инфо в поисковых системах

--- HTTP_Request/MainWindow.xaml.cs
+++ HTTP_Request/MainWindow.xaml.cs
@@ -20,9 +20,71 @@ namespace HTTP_Request
     /// </summary>
     public partial class MainWindow : Window
     {
+        string _strSearch;
+        string _strUri;
+        string _searchSystem;
         public MainWindow()
         {
             InitializeComponent();
+
+            _searchSystem = "Google";
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            if (TryParseText())
+            {
+                switch (_searchSystem)
+                {
+                    case "Google":
+                        if (Image.IsChecked == true)
+                        {
+                            _strUri = $"https://www.google.com/search?q={_strSearch}&tbm=isch";
+                        }
+                        else
+                        {
+                            _strUri = $"https://www.google.com/search?q={_strSearch}";
+                        }
+                        break;
+                    case "Bing":
+                        if (Image.IsChecked == true)
+                        {
+                            _strUri = $"https://www.bing.com/images/search?q={_strSearch}&form=HDRSC4";
+                        }
+                        else
+                        {
+                            _strUri = $"https://www.bing.com/search?q={_strSearch}";
+                        }
+                        break;
+                    default: 
+                        break;
+                }
+
+                Browser.Navigate(new Uri(_strUri));
+            }
+        }
+        private bool TryParseText()
+        {
+            if (TextSearch.Text != string.Empty)
+            {
+                string[] str = TextSearch.Text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                _strSearch = string.Join("+", str);
+
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Строка для поиска пустая!");
+                return false;
+            }
+        }
+        private void Google_Click(object sender, RoutedEventArgs e)
+        {
+            _searchSystem = "Google";
+        }
+        private void Bing_Click(object sender, RoutedEventArgs e)
+        {
+            _searchSystem = "Bing";
         }
     }
 }
